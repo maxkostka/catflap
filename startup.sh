d4a6753f89ea8a22aa52da83f91c981dfd99a81f
maxkostka <kostka.max@gmail.com>
2016-11-19 12:32:14 +0000

    launch file for training image aquisition, startup script which can be used to start the launch file, when the raspi boots up via sudo crontab -e

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+export ROS_ROOT=/opt/ros/indigo/share/ros
+export ROS_PACKAGE_PATH=/home/max/projects/catflap/ros_catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks
+export ROS_MASTER_URI=http://raspberry:11311
+export ROSLISP_PACKAGE_DIRECTORIES=/home/max/projects/catflap/ros_catkin_ws/devel/share/common-lisp
+export ROS_DISTRO=indigo
+export ROS_ETC_DIR=/opt/ros/indigo/etc/ros
+source /opt/ros/indigo/setup.bash
+source /home/max/projects/catflap/ros_catkin_ws/devel/setup.bash
+export WORKON_HOME=/home/max/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+echo switching to virtual environment cv
+workon cv
+roslaunch /home/max/projects/catflap/ros_catkin_ws/l.launch
+
